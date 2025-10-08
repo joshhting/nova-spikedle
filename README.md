6be0eb157c4a61b406ce8278ae8d8124ee152d2b
Josh Ting <joshting25@gmail.com>
2025-10-08 16:05:33 -0400

    Rename to nova-spikedle

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# nova-quordle
+# nova-spikedle
 Spellify for quotes
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- backend/load_quotes.py
+++ backend/load_quotes.py
@@ -2,18 +2,16 @@ import csv
 import io
 from pathlib import Path
 
-QUOTES_S3_BUCKET = "nova-quordle-csv"
-
 def load_quotes_from_local():
-    quotes = []
-    authors = set()
-    csv_path = "quotes.csv"
-    with open(csv_path, newline="", encoding="utf-8") as f:
-        reader = csv.DictReader(f, fieldnames=["quote", "author", "id"])
-        for row in reader:
-            if row["quote"] and row["author"] and row["quote"] != "quote":  # skip header
-                quotes.append({"quote": row["quote"], "author": row["author"]})
-                authors.add(row["author"])
-    return quotes, sorted(list(authors))
+	quotes = []
+	authors = set()
+	csv_path = "quotes.csv"
+	with open(csv_path, newline="", encoding="utf-8") as f:
+		reader = csv.DictReader(f, fieldnames=["quote", "author", "id"])
+		for row in reader:
+			if row["quote"] and row["author"] and row["quote"] != "quote":  # skip header
+				quotes.append({"quote": row["quote"], "author": row["author"]})
+				authors.add(row["author"])
+	return quotes, sorted(list(authors))
 
 
